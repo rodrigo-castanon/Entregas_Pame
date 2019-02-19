896a116adc40f54bfd2085d0276d8d89f69790ed
rodrigo-castanon <rodrigo-castanon@users.noreply.github.com>
2019-02-19 14:56:27 -0300

    Update index.js

---
+++ src/Pages/Secundaria/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+import Pesquisa from './components/Pesquisa';
+
+import Menu from './components/Menu'
+
+class App extends Component {
+  render() {
+    return (
+      <body> 
+        <Player />
+          
+      </body>
+
+    );
+  }
+}
+
+export default App;
